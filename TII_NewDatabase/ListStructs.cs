c8f96f6b24298279921da5642a072e1fcf5cfeb4
Jonathan Hartman <jonhtii@aol.com>
2014-10-14 15:23:31 -0400

    Moved List Structs to seperate file
    Moved the CompanyListItem and BuildingListItem to their own separate
    file and made them public.
    My intention is that the company and building information can be loaded
    from the database at runtime, and that child forms can access that
    information on the main form rather than regenerate it themselves.

---
+++ TII_NewDatabase/ListStructs.cs
@@ -0,0 +1,60 @@
+﻿// <summary> Structs for use in holding information pulled from the database at run-time. </summary>
+
+namespace TII_NewDatabase
+{
+    /// <summary>
+    /// Struct for holding Company Info used to sort/filter Companies in the ListBox.
+    /// </summary>
+    public struct CompanyListItem
+    {
+        /// <summary>Company Name.</summary>
+        public string Name;
+
+        /// <summary>Can be DC, MD or BOTH.</summary>
+        public string DCorMD;
+
+        /// <summary>Indicates if the Company has any active buildings associated or not.</summary>
+        public bool IsActive;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompanyListItem"/> struct.
+        /// </summary>
+        /// <param name="name">Company Name.</param>
+        /// <param name="dc_or_md">Can be DC, MD or BOTH.</param>
+        /// <param name="active">Indicates if the Company has any active buildings associated or not.</param>
+        public CompanyListItem(string name, string dc_or_md, bool active)
+        {
+            this.Name = name;
+            this.DCorMD = dc_or_md;
+            this.IsActive = active;
+        }
+    }
+
+    /// <summary>
+    /// Struct for holding Building Info used to sort/filter Buildings in the ListBox.
+    /// </summary>
+    public struct BuildingListItem
+    {
+        /// <summary>Building Address.</summary>
+        public string Address;
+
+        /// <summary>Can be DC, MD or BOTH.</summary>
+        public string DCorMD;
+
+        /// <summary>Indicates if the Company has any active buildings associated or not.</summary>
+        public bool IsActive;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BuildingListItem"/> struct.
+        /// </summary>
+        /// <param name="address">Building Address.</param>
+        /// <param name="dc_or_md">Can be DC or MD.</param>
+        /// <param name="active">Indicates if the building is listed as active.</param>
+        public BuildingListItem(string address, string dc_or_md, bool active)
+        {
+            this.Address = address;
+            this.DCorMD = dc_or_md;
+            this.IsActive = active;
+        }
+    }
+}

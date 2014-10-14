e1933eb987e2d05838fd2020ed415ad50f11b0d5
Jonathan Hartman <jonhtii@aol.com>
2014-10-14 15:03:18 -0400

    Added the start of a form to Insert Inspections

---
+++ TII_NewDatabase/AddNewForms/FormAddInspection.cs
@@ -0,0 +1,22 @@
+﻿// <summary> Creates a form able to insert inspection information into the database. </summary>
+
+namespace TII_NewDatabase.AddNewForms
+{
+    using System;
+    using System.Data;
+    using System.Windows.Forms;
+    using Database;
+    using SQL;
+
+    /// <summary> Form for entering inspection information. </summary>
+    public partial class FormAddInspection : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FormAddInspection"/> class.
+        /// </summary>
+        public FormAddInspection()
+        {
+            this.InitializeComponent();
+        }
+    }
+}

b1db4952ffabb5c58da7fc4c08357b7755fe4d00
Jonathan Hartman <jonhtii@aol.com>
2014-11-05 15:49:56 -0500

    Bug Fix in SQLColumn
    When a column was created using SQLColumn(string, string), instead of
    assigning "NULL" to the SQLColumn.Value, it was assigned to the
    parameter passed in, with the result that SQLColumn.Value remained null.
    This would result in improperly formatted SQL Queries.

--- Database_Library/SQLColumn.cs
+++ Database_Library/SQLColumn.cs
@@ -66,7 +66,7 @@ namespace Database
         {
             if (value == null)
             {
-                value = "NULL";
+                this.Value = "NULL";
             }
             else
             {

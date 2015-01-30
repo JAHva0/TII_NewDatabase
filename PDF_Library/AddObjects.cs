1f9a7287e9f6388509be1a6e2341cf42f38e704d
Jonathan Hartman <jonhtii@aol.com>
2015-01-30 14:14:20 -0500

    Laid the groundwork for PDF creation
    Eventually the idea would be to allow the user to create a blank
    certificate from the Add Inspection form if the inspection was both DC
    and clean.

--- PDF_Library/AddObjects.cs
+++ PDF_Library/AddObjects.cs
@@ -3,32 +3,19 @@
 namespace PDF_Library
 {
     using System;
-    using System.Diagnostics;
     using System.Drawing;
     using System.IO;
     using iTextSharp.text;
     using iTextSharp.text.pdf;
-    
+
     /// <summary> Provides static methods for PDF related functions. </summary>
     public static partial class PDF
     {
-        
-    }
-
-    public class PDF_Document
-    {
-        private Document doc;
-
-        public PDF_Document(string filename)
-        {
-            PdfReader reader = new PdfReader(filename);
-
-            doc = new Document(reader.GetPageSizeWithRotation(1));
-        }
-
-        public void AddText(string text, Point location)
+        public static void AddImage(string inputfile, string outputfile, string imagefile, Point imagePosition, Size imageSize)
         {
             
         }
     }
+
+
 }
